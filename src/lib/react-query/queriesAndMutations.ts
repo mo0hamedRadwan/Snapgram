2cfba0c232ac485ca72e6b51d722f503363a6b4e
mo0hamedRadwan <mo0hamedradwan@yahoo.com>
2024-03-08 06:04:59 +0200

    fixed some ts problems

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -168,8 +168,10 @@ export const useGetPosts = () => {
         queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
         queryFn: getInfinitePosts,
         getNextPageParam: (lastPage) => {
-            if (lastPage && lastPage.documents.length === 0) return null;
-            const lastId = lastPage?.documents[lastPage.documents.length - 1].$id;
+            if(!lastPage)return undefined;
+            if (lastPage && lastPage.length === 0) return null;
+
+            const lastId = lastPage[lastPage.length - 1].$id;
             return lastId;
         }
     })
